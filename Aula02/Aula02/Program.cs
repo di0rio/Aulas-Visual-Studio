f3aef421739235278f9ed9d712869a135742fc04
Sesi3 <Sesi3@AI11-1264978>
2024-02-01 10:19:26 -0300

    Adicionar arquivos de projeto.

---
+++ Aula02/Aula02/Program.cs
@@ -0,0 +1,21 @@
+﻿using Aula02;
+
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        Personagem p1 = new Personagem("Gandalf", "Mago", 1, 10, 7, 10, 100);
+        p1.ApresentarSe();
+
+        Personagem p2 = new Personagem("Legolas", "Arqueiro", 1, 5, 20, 5, 100);
+        p2.ApresentarSe();
+
+        Personagem p3 = new Personagem("Gimli", "Guerreiro", 3, 20, 30, 10, 100);
+        p3.ApresentarSe();
+
+        Personagem p4 = new Personagem("Aragorn", "Guerreiro", 4, 30, 20, 25, 100);
+        p4.ApresentarSe();
+
+        p1.Duelo(p3);
+    }
+}
