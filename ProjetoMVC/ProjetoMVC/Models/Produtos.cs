e5125824793bb1635e3310895361e27c590d4896
Cauã Diório <caua.diorio29@gmail.com>
2024-02-01 16:27:42 -0300

    Criando lista com edit, delete e details

---
+++ ProjetoMVC/ProjetoMVC/Models/Produtos.cs
@@ -0,0 +1,18 @@
+﻿namespace ProjetoMVC.Models
+{
+	public class Produtos
+	{
+		public int Id { get; set; }
+
+		public string Name { get; set; }
+
+		public double Price { get; set; }
+
+		public Produtos(int id, string name, double price)
+		{
+			Id = id;
+			Name = name;
+			Price = price;
+		}
+	}
+}
